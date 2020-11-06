5e2c61b8e0acd10c33162866973b75ef1dde9c90
eric alcaraz <eric.alcaraz@protonmai.com>
2020-11-06 12:53:32 +0100

    Model starts to learn but very slowly (loss decrease and acc increase)

--- model.py
+++ model.py
@@ -1,25 +1,65 @@
 import tensorflow
 from tensorflow import keras
 from tensorflow.keras import layers
-from generate_data import *
+from generate_data import generate_data
+import matplotlib.pyplot as plt
 
 img_size = 224
 batch_size = 128
-
+epochs = 30
 
 inputs = keras.Input(shape=(img_size, img_size, 3))
 
 x = layers.Conv2D(16, (3, 3), padding='same', activation='relu')(inputs)
 x = layers.MaxPooling2D(pool_size=(2, 2))(x)
-x = layers.Conv2D(32, (3, 3), padding='same', activation='relu')(x)
+# x = layers.Conv2D(32, (3, 3), padding='same', activation='relu')(x)
+# x = layers.MaxPooling2D(pool_size=(2, 2))(x)
+# x = layers.Conv2D(64, (3, 3), padding='same', activation='relu')(x)
+# x = layers.MaxPooling2D(pool_size=(2, 2))(x)
+# x = layers.Conv2D(32, (3, 3), padding='same', activation='relu')(x)
+# x = layers.MaxPooling2D(pool_size=(2, 2))(x)
+# x = layers.Conv2D(16, (3, 3), padding='same', activation='relu')(x)
 x = layers.MaxPooling2D(pool_size=(2, 2))(x)
 x = layers.Flatten()(x)
-x = layers.Droput(0.2)(x)
+#x = layers.Dropout(0.2)(x)
 x = layers.Dense(512, activation='relu')(x)
 # Get 225 from split_data
 output = layers.Dense(225, activation='softmax')(x)
 
 model = keras.Model(inputs, output, name='bird_classifier')
 
+# from_logits=True
+model.compile(
+    loss=keras.losses.SparseCategoricalCrossentropy(),
+    optimizer=keras.optimizers.SGD(lr=0.0001),
+    metrics=["accuracy"]
+)
+
 train_data_generated, valid_data_generated, test_data_generated = generate_data(
     img_size, batch_size)
+
+history = model.fit(train_data_generated, validation_data=valid_data_generated,
+                    epochs=epochs, verbose=2)
+
+# Plot results
+acc = history.history['accuracy']
+# val_acc = history.history['val_acc']
+
+# loss = history.history['loss']
+# val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+# plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+# plt.subplot(1, 2, 2)
+# plt.plot(epochs_range, loss, label='Training Loss')
+# # plt.plot(epochs_range, val_loss, label='Validation Loss')
+# plt.legend(loc='upper right')
+# plt.title('Training and Validation Loss')
+plt.show()
